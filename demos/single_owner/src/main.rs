3647ff9db973137b7687b5ebb5f233903dc5264b
Remo Senekowitsch <remo@buenzli.dev>
2024-10-04 07:44:54 +0200

    turn single ownership slides into demo

---
+++ demos/single_owner/src/main.rs
@@ -0,0 +1,15 @@
+//
+//
+//
+fn main() {
+    //                heap-allocated String
+    //                vvvvvvvvvvvvvvvvvvvvv
+    let first_owner = String::from("hello");
+
+    // let second_owner = first_owner;
+
+    println!("{:?}, world!", first_owner);
+}
+
+// #[derive(Debug, Clone)]
+// struct Foo;
